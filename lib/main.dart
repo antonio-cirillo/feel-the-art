82c18a058517c01eb9686a61b157858db4f2d6ca
Antonio Cirillo <dev.cirillo@gmail.com>
2022-11-06 18:47:15 +0100

    changed home with menu widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
-import 'package:feel_the_art/screens/welcome/welcome_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'package:feel_the_art/screens/menu/menu_screen.dart';
+import 'package:feel_the_art/utils/colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,10 +13,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(),
-      home: const WelcomeScreen()
-    );
+        title: 'Feel the ART',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(scaffoldBackgroundColor: kPrimaryColor),
+        home: const Menu());
   }
 }
