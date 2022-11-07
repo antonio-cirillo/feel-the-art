d2c6201169374b365177698a4e2e5c536f6dc9db
Antonio Cirillo <dev.cirillo@gmail.com>
2022-11-07 15:44:03 +0100

    added profile and collection buttons

--- lib/screens/menu/menu_screen.dart
+++ lib/screens/menu/menu_screen.dart
@@ -1,4 +1,6 @@
+import 'package:feel_the_art/screens/account/account_screen.dart';
 import 'package:feel_the_art/screens/collection/collection_screen.dart';
+import 'package:feel_the_art/screens/daily_question/daily_question_screen.dart';
 import 'package:feel_the_art/screens/home_page/home_page_screen.dart';
 import 'package:feel_the_art/utils/colors.dart';
 import 'package:flutter/cupertino.dart';
@@ -22,20 +24,37 @@ class MenuState extends State<Menu> {
   }
 
   List<Widget> _buildScreens() {
-    return [const HomePage(), const Collection()];
+    return [
+      const HomePage(),
+      const Collection(),
+      const DailyQuestion(),
+      const Account()
+    ];
   }
 
   List<PersistentBottomNavBarItem> _navBarsItems() {
     return [
       PersistentBottomNavBarItem(
-        icon: const Icon(CupertinoIcons.home),
+        icon: const Icon(CupertinoIcons.game_controller),
         title: ("Home"),
         activeColorPrimary: kPrimaryColor,
         inactiveColorPrimary: CupertinoColors.systemGrey,
       ),
       PersistentBottomNavBarItem(
         icon: const Icon(CupertinoIcons.search),
-        title: ("Search"),
+        title: ("Collection"),
+        activeColorPrimary: kPrimaryColor,
+        inactiveColorPrimary: CupertinoColors.systemGrey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: const Icon(CupertinoIcons.question_square),
+        title: ("Question"),
+        activeColorPrimary: kPrimaryColor,
+        inactiveColorPrimary: CupertinoColors.systemGrey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: const Icon(CupertinoIcons.person_crop_circle),
+        title: ("Account"),
         activeColorPrimary: kPrimaryColor,
         inactiveColorPrimary: CupertinoColors.systemGrey,
       )
