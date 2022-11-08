3f794050bb384bd3660e59d93ac3af50601afc2f
Antonio Cirillo <dev.cirillo@gmail.com>
2022-11-08 17:56:32 +0100

    added CardTableSlider's widget

--- lib/screens/home_page/home_page_screen.dart
+++ lib/screens/home_page/home_page_screen.dart
@@ -1,3 +1,4 @@
+import 'package:feel_the_art/screens/home_page/components/card_table_slider.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,6 +6,14 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+        body: SizedBox(
+            height: size.height,
+            width: double.infinity,
+            child: Stack(
+              alignment: Alignment.center,
+              children: const <Widget>[Positioned(child: CardTableSlider())],
+            )));
   }
 }
