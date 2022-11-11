552efb589ce41d87c9450cfd6b37dd63b6fa3375
Antonio Cirillo <dev.cirillo@gmail.com>
2022-11-11 23:08:00 +0100

    moved the handling of body

--- lib/screens/home_page/home_page_screen.dart
+++ lib/screens/home_page/home_page_screen.dart
@@ -1,19 +1,14 @@
-import 'package:feel_the_art/screens/home_page/components/card_table_slider.dart';
+import 'package:feel_the_art/utils/size_config.dart';
 import 'package:flutter/material.dart';
+
+import 'components/body.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Scaffold(
-        body: SizedBox(
-            height: size.height,
-            width: double.infinity,
-            child: Stack(
-              alignment: Alignment.center,
-              children: const <Widget>[Positioned(child: CardTableSlider())],
-            )));
+    SizeConfig().init(context);
+    return const Scaffold(body: Body());
   }
 }
