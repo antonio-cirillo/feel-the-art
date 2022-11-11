31041c99d075cf3a1f41b1c721de881db1b0645a
Antonio Cirillo <dev.cirillo@gmail.com>
2022-11-11 23:04:59 +0100

    added name of card table

--- lib/screens/home_page/components/card_table_slider.dart
+++ lib/screens/home_page/components/card_table_slider.dart
@@ -1,4 +1,5 @@
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:feel_the_art/utils/size_config.dart';
 import 'package:flutter/material.dart';
 
 final List<String> imgList = [
@@ -9,61 +10,41 @@ final List<String> imgList = [
 class CardTableSlider extends StatelessWidget {
   const CardTableSlider({Key? key}) : super(key: key);
 
-  List<Widget> _buildCards(Size size) {
+  List<Widget> _buildCards() {
     return imgList
         .map((item) => Container(
-          margin: const EdgeInsets.all(5.0),
-          child: ClipRRect(
-              borderRadius: const BorderRadius.all(Radius.circular(5.0)),
-              child: Stack(
-                children: <Widget>[
-                  Image.asset(item, fit: BoxFit.cover),
-                  Positioned(
-                    bottom: 0.0,
-                    left: 0.0,
-                    right: 0.0,
-                    child: Container(
-                      decoration: const BoxDecoration(
-                        gradient: LinearGradient(
-                          colors: [
-                            Color.fromARGB(200, 0, 0, 0),
-                            Color.fromARGB(0, 0, 0, 0)
-                          ],
-                          begin: Alignment.bottomCenter,
-                          end: Alignment.topCenter,
-                        ),
-                      ),
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 10.0, horizontal: 20.0),
-                      child: Text(
-                        'No. ${imgList.indexOf(item)} image',
-                        style: const TextStyle(
+            // color: Colors.red,
+            width: double.infinity,
+            child: Column(
+              children: <Widget>[
+                const Spacer(),
+                FittedBox(
+                  fit: BoxFit.fitWidth,
+                  child: Text("Tavolo da gioco",
+                      style: TextStyle(
                           color: Colors.white,
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              )),
-        ))
+                          fontSize: SizeConfig.getProportionateScreenWidth(36),
+                          fontFamily: 'ElsieSwashCaps')),
+                ),
+                SizedBox(height: SizeConfig.getProportionateScreenHeight(10)),
+                Stack(children: <Widget>[Image.asset(item)]),
+                const Spacer(flex: 2)
+              ],
+            )))
         .toList();
   }
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
     return CarouselSlider(
       options: CarouselOptions(
-        height: size.height * 0.8,
-        autoPlay: false,
-        enableInfiniteScroll: false,
-        initialPage: 0,
-        aspectRatio: 2.0,
-        enlargeCenterPage: true,
-      ),
-      items: _buildCards(size),
+          height: SizeConfig.getProportionateScreenHeight(650),
+          autoPlay: false,
+          enableInfiniteScroll: false,
+          initialPage: 0,
+          enlargeCenterPage: true,
+          viewportFraction: 0.75),
+      items: _buildCards(),
     );
   }
 }
