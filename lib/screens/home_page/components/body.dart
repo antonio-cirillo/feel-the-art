1ed2c7d9a2b1fab2de8be69da3de0ac5edf2cb5a
Antonio Cirillo <dev.cirillo@gmail.com>
2022-11-11 23:06:19 +0100

    creator of home page's body

---
+++ lib/screens/home_page/components/body.dart
@@ -0,0 +1,25 @@
+import 'package:feel_the_art/screens/home_page/components/card_table_slider.dart';
+import 'package:flutter/material.dart';
+
+class Body extends StatefulWidget {
+  const Body({super.key});
+
+  @override
+  State<Body> createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Column(
+          children: <Widget>[
+            Expanded(flex: 2, child: Container(color: Colors.black)),
+            const Expanded(
+                flex: 7,
+                child: Align(
+                    alignment: Alignment.center, child: CardTableSlider()))
+          ],
+        ));
+  }
+}
