0016350d39b636de03d8748cb6d739c2f5007fd3
Antonio Cirillo <dev.cirillo@gmail.com>
2022-12-18 18:27:37 +0100

    updated account screen

--- app/lib/screens/account/account_screen.dart
+++ app/lib/screens/account/account_screen.dart
@@ -1,28 +1,38 @@
+import 'package:feel_the_art/utils/theme/size_config.dart';
 import 'package:flutter/material.dart';
 
-import '../../utils/theme/size_config.dart';
+import '../../components/background.dart';
+import '../../utils/theme/colors.dart';
 import 'components/avatar.dart';
-import 'components/chart.dart';
-import 'components/statistics.dart';
 
 class AccountScreen extends StatelessWidget {
   const AccountScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    double padding = MediaQuery.of(context).getProportionateScreenWidth(20);
-    return Scaffold(
-      body: Padding(
-          padding: EdgeInsets.fromLTRB(padding, padding, padding, 0),
-          child: SingleChildScrollView(
-              child: Column(
-            children: <Widget>[
-              const Avatar(),
-              Divider(height: padding),
-              const Statistics(),
-              const ChartWin()
-            ],
-          ))),
-    );
+    double proportionate40px =
+        MediaQuery.of(context).getProportionateScreenHeight(40);
+    return Stack(children: [
+      Container(color: amethystColor.withOpacity(0.8)),
+      const BackgroundScreen(),
+      Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Column(
+          children: [
+            SizedBox(height: proportionate40px),
+            const Expanded(
+                flex: 3,
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Avatar(),
+                )),
+            const Expanded(
+              flex: 4,
+              child: Text("Ciao")
+            )
+          ],
+        ),
+      )
+    ]);
   }
 }
