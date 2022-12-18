aec23356ba91515841d99f10812e500678176650
Antonio Cirillo <dev.cirillo@gmail.com>
2022-12-18 18:27:19 +0100

    refactored code

--- app/lib/screens/collection/components/grid_cards_widget.dart
+++ app/lib/screens/collection/components/grid_cards_widget.dart
@@ -17,17 +17,25 @@ class GridCards extends StatefulWidget {
 
 class _GridCardsState extends State<GridCards> {
   @override
-  Widget buildAnimatedItem(BuildContext context, int index, Animation<double> animation) => FadeTransition(
+  Widget buildAnimatedItem(
+          BuildContext context, int index, Animation<double> animation) =>
+      FadeTransition(
         opacity: Tween<double>(begin: 0, end: 1).animate(animation),
         child: SlideTransition(
-          position: Tween<Offset>(begin: const Offset(0, -0.1), end: Offset.zero).animate(animation),
+          position:
+              Tween<Offset>(begin: const Offset(0, -0.1), end: Offset.zero)
+                  .animate(animation),
           child: Padding(
-            padding: EdgeInsets.only(bottom: MediaQuery.of(context).getProportionateScreenHeight(10)),
+            padding: EdgeInsets.only(
+                bottom:
+                    MediaQuery.of(context).getProportionateScreenHeight(10)),
             child: OpenContainer(
-              openBuilder: (context, _) => CardScreen(widget.deckListModel.get(0).deck.elementAt(index)),
+              openBuilder: (context, _) =>
+                  CardScreen(widget.deckListModel.get(0).deck.elementAt(index)),
               closedColor: Colors.transparent,
               closedElevation: 0,
-              closedBuilder: (context, openContainer) => Image.asset(widget.deckListModel.get(0).deck.elementAt(index).image),
+              closedBuilder: (context, openContainer) => Image.asset(
+                  widget.deckListModel.get(0).deck.elementAt(index).image),
             ),
           ),
         ),
@@ -35,14 +43,16 @@ class _GridCardsState extends State<GridCards> {
 
   @override
   Widget build(BuildContext context) {
-      return LiveGrid(
-          itemBuilder: buildAnimatedItem,
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            childAspectRatio: 0.6,
-            crossAxisCount: 3,
-            crossAxisSpacing: MediaQuery.of(context).getProportionateScreenHeight(20),
-            // mainAxisSpacing: MediaQuery.of(context).getProportionateScreenHeight(20),
-          ),
-          itemCount: widget.deckListModel.get(0).deck.length);
+    return LiveGrid(
+        physics: const BouncingScrollPhysics(),
+        itemBuilder: buildAnimatedItem,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          childAspectRatio: 0.6,
+          crossAxisCount: 3,
+          crossAxisSpacing:
+              MediaQuery.of(context).getProportionateScreenHeight(20),
+          // mainAxisSpacing: MediaQuery.of(context).getProportionateScreenHeight(20),
+        ),
+        itemCount: widget.deckListModel.get(0).deck.length);
   }
 }
