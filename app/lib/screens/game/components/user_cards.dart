0540f91f21e5f95866280f439a6c9db0b4436523
Antonio Cirillo <dev.cirillo@gmail.com>
2022-12-25 22:11:56 +0100

    init round

--- app/lib/screens/game/components/user_cards.dart
+++ app/lib/screens/game/components/user_cards.dart
@@ -2,38 +2,28 @@ import 'package:feel_the_art/theme/size_config.dart';
 import 'package:flutter/material.dart';
 
 class UserCardsScreen extends StatelessWidget {
-  const UserCardsScreen({super.key});
+  bool played;
+  List<int> listCards;
 
+  UserCardsScreen({super.key, this.played = false, required this.listCards});
+
+  @override
   @override
   Widget build(BuildContext context) {
     double imageHeight = MediaQuery.of(context).screenHeight * 0.21;
     return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Draggable(
-          data: 0,
-          feedback: Container(
-              height: 150,
-              width: 150,
-              color: Colors.orange),
-          child: Container(
-              height: 150,
-              width: 150,
-              color: Colors.orange),
-          childWhenDragging: Container(),
-        ),
-        Draggable(
-            data: 0,
-            feedback: Container(
-              child: Image.asset('images/0.png'),
-              height: imageHeight,
-            ),
-            child: Container(
-                margin: EdgeInsets.symmetric(horizontal: 10),
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: listCards.map((e) {
+          return Draggable(
+              maxSimultaneousDrags: (played) ? 0 : 1,
+              data: 0,
+              feedback: SizedBox(
                 height: imageHeight,
-                child: Image.asset('images/0.png')),
-            childWhenDragging: Container())
-      ],
-    );
+                child: Image.asset('images/$e.png'),
+              ),
+              childWhenDragging: Container(),
+              child: SizedBox(
+                  height: imageHeight, child: Image.asset('images/$e.png')));
+        }).toList());
   }
 }
--- app/lib/screens/game/game_screen.dart
+++ app/lib/screens/game/game_screen.dart
@@ -1,11 +1,31 @@
+import 'package:feel_the_art/screens/game/components/user_cards.dart';
 import 'package:feel_the_art/theme/theme.dart';
 import 'package:flutter/material.dart';
 
-import 'components/table_cards.dart';
-import 'components/user_hand.dart';
+import 'components/information.dart';
 
-class GameScreen extends StatelessWidget {
+class GameScreen extends StatefulWidget {
   const GameScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _GameScreenState();
+}
+
+class _GameScreenState extends State<GameScreen> {
+  final bool played = false;
+  List<int> listCards = [];
+
+  @override
+  void initState() {
+    for (var i = 1; i < 6; i++) {
+      Future.delayed(Duration(seconds: 1 * i + 2), () {
+        setState(() {
+          listCards.add(0);
+        });
+      });
+    }
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,14 +36,18 @@ class GameScreen extends StatelessWidget {
             body: Column(
               children: [
                 Expanded(flex: 2, child: Container()),
+                Expanded(flex: 4, child: Container()),
                 Expanded(
-                    flex: 4,
-                    child: Row(children: [
-                      const Spacer(flex: 2),
-                      Expanded(flex: 5, child: TableCardsScreen()),
-                      const Spacer(flex: 2)
-                    ])),
-                Expanded(flex: 3, child: UserHandScreen())
+                    flex: 3,
+                    child: Column(children: [
+                      Expanded(
+                          flex: 6,
+                          child: UserCardsScreen(
+                            played: played,
+                            listCards: listCards,
+                          )),
+                      const Expanded(flex: 3, child: InformationScreen())
+                    ]))
               ],
             )),
         Positioned(top: 0, child: Image.asset('background/game_bg.png'))
